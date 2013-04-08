b930c6577bd8160ffec3e5a1db17e248eb0e09a5
prembhaskal <pbhaskal@gmail.com>
2013-04-09 01:33:12 +0530

    exceeding time limit. find a linear solution.

---
+++ code_pratice_ws/pratice_code/src/codechef/april13/king_con/TestTaskA.java
@@ -0,0 +1,22 @@
+package codechef.april13.king_con;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class TestTaskA {
+
+	public static void main(String[] s) {
+		Scanner in = new Scanner(TestTaskA.class.getResourceAsStream("test.txt"));
+		PrintWriter out = new PrintWriter(System.out);
+
+		long startTime = System.nanoTime();
+
+		new TaskA().solve(in, out);
+
+		long endTime = System.nanoTime();
+		out.println("elapsed time " + (endTime-startTime)/1000000 + "milli secs");
+
+		in.close();
+		out.flush();
+	}
+}
