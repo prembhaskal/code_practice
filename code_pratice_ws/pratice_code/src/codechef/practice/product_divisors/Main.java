453543281a4f8512a3044778e53403b30978fc49
prembhaskal <pbhaskal@gmail.com>
2013-04-27 19:31:27 +0530

    starting this task

---
+++ code_pratice_ws/pratice_code/src/codechef/practice/product_divisors/Main.java
@@ -0,0 +1,34 @@
+package codechef.practice.product_divisors;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] s) {
+		Scanner in = new Scanner(System.in);
+		PrintWriter writer = new PrintWriter(System.out);
+
+		TaskA solution = new TaskA();
+		solution.solve(in,writer);
+
+		writer.close();
+		in.close();
+	}
+
+}
+
+// product of divisors..... here is the http://ww2.codechef.com/problems/D1/
+class TaskA {
+
+	public void solve(Scanner in, PrintWriter out) {
+		int tests = Integer.parseInt(in.next());
+
+		for (int i=0;i<tests;i++) {
+
+		}
+
+	}
+
+}
+
