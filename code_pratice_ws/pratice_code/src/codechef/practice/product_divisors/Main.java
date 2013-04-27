806236798263ecea5e3b7da1510664ae2ec60eee
prembhaskal <pbhaskal@gmail.com>
2013-04-28 02:02:33 +0530

    task submitted after using bufferedReader instead of scanner.

--- code_pratice_ws/pratice_code/src/codechef/practice/product_divisors/Main.java
+++ code_pratice_ws/pratice_code/src/codechef/practice/product_divisors/Main.java
@@ -1,19 +1,26 @@
 package codechef.practice.product_divisors;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] s) {
-		Scanner in = new Scanner(System.in);
-		PrintWriter writer = new PrintWriter(System.out);
+		try {
+			BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+			PrintWriter writer = new PrintWriter(System.out);
 
-		TaskA solution = new TaskA();
-		solution.solve(in,writer);
+			TaskA solution = new TaskA();
+			solution.solve(in,writer);
 
-		writer.close();
-		in.close();
+			writer.close();
+
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+		}
 	}
 
 }
@@ -21,13 +28,182 @@ public class Main {
 // product of divisors..... here is the http://ww2.codechef.com/problems/D1/
 class TaskA {
 
-	public void solve(Scanner in, PrintWriter out) {
-		int tests = Integer.parseInt(in.next());
+	int MOD = 10000;
+
+	boolean isMoreThanMod;
+
+	List<Integer> primeList = new ArrayList<Integer>();
+
+	static Map<Integer, Integer> squareAndRootMap = new HashMap<Integer, Integer>();
+
+	static Map<Integer, String> numVsDivisor = new HashMap<Integer, String>();
+
+	static boolean isInitialized = false;
+
+	static int[] primenos = new int[] {2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,
+			101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,
+			211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,
+			307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,
+			401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,
+			503,509,521,523,541,547,557,563,569,571,577,587,593,599,
+			601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709};
+
+	public void solve(BufferedReader in, PrintWriter out) throws IOException {
+
+		init();
+		int tests = Integer.parseInt(in.readLine());
+
+		int[] nos = new int[tests];
 
 		for (int i=0;i<tests;i++) {
-
+			nos[i] = Integer.parseInt(in.readLine());
 		}
 
+		for (int i=0;i<tests;i++) {
+			String digits = getDivisorProd4Digits(nos[i]);
+			out.println(digits);
+		}
+
+	}
+
+
+	private String getDivisorProd4Digits(int num) {
+		String fourDigits = "";
+		isMoreThanMod = false;
+
+		long divisorsProd = 1;
+
+		int numberOfDivisors = getNumberOfDivisors(num);
+
+		int root = (int) Math.sqrt(num);
+
+//		if (numberOfDivisors%2!=0) {
+//			divisorsProd = power(root, numberOfDivisors);
+//		} else {
+//			divisorsProd = power(num, numberOfDivisors/2);
+//		}
+//
+		Integer squareRoot = squareAndRootMap.get(num);
+		if (squareRoot!=null) {
+			divisorsProd = (power(num, (numberOfDivisors-1)/2) * squareRoot);
+			if (divisorsProd >= MOD)
+				isMoreThanMod = true;
+			divisorsProd = divisorsProd%MOD;
+		} else {
+			divisorsProd = power(num, numberOfDivisors/2);
+		}
+
+		fourDigits = "" + divisorsProd;
+
+		if (!isMoreThanMod) {
+			return fourDigits;
+		}
+		else {
+			fourDigits = String.valueOf(divisorsProd + MOD);
+			fourDigits = fourDigits.substring(1);
+			return fourDigits;
+		}
+
+	}
+
+	private int getNumberOfDivisors(int num) {
+		int numberOfDivisors = 1;
+
+		for (int i=0;i<primenos.length;i++) {
+			int prime = primenos[i];
+
+			if (prime > num/prime)
+				break;
+
+			if (num%prime==0) {
+				int k=1;
+				while (num%prime==0) {
+					num /= prime;
+					k++;
+				}
+
+				numberOfDivisors = numberOfDivisors * k;
+			}
+		}
+
+		if (num > 1) {
+			numberOfDivisors = numberOfDivisors * 2;
+		}
+
+		// 1 and the number itself should not be considered.
+		numberOfDivisors = numberOfDivisors - 2;
+
+		return numberOfDivisors;
+	}
+
+
+	private int power(long num, long pow) {
+		long power = 1;
+
+		while (pow > 0) {
+			if (pow%2==1){
+				power = (power * num);
+				if (power >= MOD) {
+					isMoreThanMod = true;
+					power = power % MOD;
+				}
+			}
+
+			num = (num * num);
+			if (num >= MOD && pow > 1) {
+				isMoreThanMod = true;
+				num = num % MOD;
+			}
+			pow /= 2;
+		}
+
+		return (int) power;
+	}
+
+
+	private void init() {
+		if (isInitialized)
+			return;
+
+		isInitialized = true;
+
+		// get squares and roots
+		int square = 1;
+		for (int i=1;square<=500000;i++) {
+			square = i*i;
+			squareAndRootMap.put(square, i);
+		}
+//
+//		for (int i=1;i<=500000;i++) {
+//			String prodDigits = getDivisorProd4Digits(i);
+//			numVsDivisor.put(i,prodDigits);
+//		}
+
+	}
+
+
+	private void generatePrimes() {
+		int num = 3;
+		primeList.add(2);
+
+		while (num <= 500000/num) {
+			boolean isPrime = true;
+			for (int prime : primeList) {
+				if (prime > num/prime) {
+					break;
+				}
+
+				if (num%prime==0) {
+					isPrime = false;
+					break;
+				}
+			}
+
+			if (isPrime)
+				primeList.add(num);
+
+			num += 2;
+		}
 	}
 
 }
--- code_pratice_ws/pratice_code/src/codechef/practice/product_divisors/TestTaskA.java
+++ code_pratice_ws/pratice_code/src/codechef/practice/product_divisors/TestTaskA.java
@@ -1,17 +1,25 @@
 package codechef.practice.product_divisors;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class TestTaskA {
 
-	public static void main(String[] s) {
-		Scanner in = new Scanner(TestTaskA.class.getResourceAsStream("test.txt"));
+	public static void main(String[] s) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(TestTaskA.class.getResourceAsStream("test.txt")));
 		PrintWriter out = new PrintWriter(System.out);
+
+		long startTime = System.nanoTime();
 
 		new TaskA().solve(in, out);
 
+		long endTime = System.nanoTime();
+		out.println("elapsed time " + (endTime - startTime) / 1000000 + "milli secs");
+
 		in.close();
-		out.close();
+		out.flush();
 	}
 }
