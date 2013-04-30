6a603dd602cdb7915dd775b00c509973a3ad5ee5
prembhaskal <pbhaskal@gmail.com>
2013-04-30 19:18:25 +0530

    StreamTokenizer is truncating "long" inputs, since it is converting double to integer, so dumping it for good.

---
+++ code_pratice_ws/pratice_code/src/common/util/InputReader.java
@@ -0,0 +1,46 @@
+package common.util;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * User: premkumar.bhaskal
+ * Date: 4/30/13
+ * Time: 3:53 PM
+ *
+ *
+ * Class to read the input faster
+ */
+public class InputReader {
+	public BufferedReader reader;
+	public StringTokenizer tokenizer;
+
+	public InputReader(InputStream stream) {
+		reader = new BufferedReader(new InputStreamReader(stream));
+		tokenizer = null;
+	}
+
+	public String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong() {
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble() {
+		return Double.parseDouble(next());
+	}
+
+}
---
+++ code_pratice_ws/pratice_code/src/google_code_jam/cjam_13/Bullseye.java
@@ -0,0 +1,107 @@
+package google_code_jam.cjam_13;
+
+import common.util.InputReader;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * User: premkumar.bhaskal
+ * Date: 4/30/13
+ * Time: 4:02 PM
+ */
+public class Bullseye {
+
+	private long lowerLimit;
+	private long upperLimit;
+
+
+	public void solve(Scanner in, PrintWriter out) throws IOException {
+		int tests = in.nextInt();
+
+		for (int testNo=1;testNo<=tests;testNo++) {
+			long radius = in.nextLong();
+			long totalPaint = in.nextLong();
+
+//			long maxRings = getMaxBlackRings(radius, totalPaint);
+			long maxRings = getMaxBlackRingsBigData(radius, totalPaint);
+
+			out.println("Case #" + testNo + ": " + maxRings);
+		}
+
+	}
+
+	// this method is smaller inputs only
+	// area of a strip of inner radius r and outer radius r+1 is PI*(2*r+1)
+	// area of the next strip is PI * (2*r+5), then PI*(2*r+9) and so on.
+	public long getMaxBlackRings(long radius, long totalPaint) {
+		long noOfRings = 0;
+
+		long paintReqd = (2*radius + 1);
+
+		while (paintReqd <= totalPaint) {
+			noOfRings++;
+
+			paintReqd += (2 * radius + 1 + 4*noOfRings);
+		}
+
+		return noOfRings;
+	}
+
+	// to test for bigger numbers
+	// total paint for n circles with initial radius 'r' == 2nr + 2n^2 - n;
+	public long getMaxBlackRingsBigData(long radius, long totalPaint) {
+		findoutLowerAndUpperLimit(radius, totalPaint);
+		long rings = getMaxRingsFromLimits(radius, totalPaint);
+//		System.out.println("lower limit is " + lowerLimit + " and upper limit is " + upperLimit);
+		return rings;
+	}
+
+	private long getMaxRingsFromLimits(long radius, long totalPaint) {
+		while (upperLimit - lowerLimit > 1) {
+			long mid = (upperLimit + lowerLimit)/2;
+
+			long paintReqd = getPaintRequired(radius, mid);
+
+			if (paintReqd > totalPaint) {
+				upperLimit = mid;
+			} else {
+				lowerLimit = mid;
+			}
+		}
+
+		return lowerLimit;
+
+	}
+
+	private boolean isPaintReqdMoreThanTotalPaint(long radius, long num, long totalPaint) {
+		double paintReqd = (double)num * (2*radius + 2*num - 1);
+		if (paintReqd > Long.MAX_VALUE || (long)paintReqd > totalPaint)
+			return true;
+		return false;
+	}
+
+	private void findoutLowerAndUpperLimit(long radius, long totalPaint) {
+		lowerLimit = 1;
+		upperLimit = 1;
+
+		long num = 1;
+		long paintRequired = 0;
+		while (paintRequired <= totalPaint) {
+			lowerLimit = num;
+			num = num * 2;
+			paintRequired = getPaintRequired(radius, num);
+		}
+
+		upperLimit = num;
+	}
+
+	private long getPaintRequired(long radius, long num) {
+		double paintReqd = (2*radius + 2*num - 1);
+		paintReqd = paintReqd * (double)num;
+		if (paintReqd > Long.MAX_VALUE)
+			System.out.println("EXCEEDING RANGE :(");
+		return num * (2*radius + 2*num -1);
+	}
+}
