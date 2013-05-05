74484430aa37235291e517ad889522a12f87cff5
prembhaskal <pbhaskal@gmail.com>
2013-05-06 03:02:06 +0530

    checking primes

---
+++ code_pratice_ws/pratice_code/src/practice/math_problems/CheckPrime.java
@@ -0,0 +1,10 @@
+package practice.math_problems;
+
+import java.math.BigInteger;
+
+public class CheckPrime {
+
+	public boolean isPrime(long prime) {
+		BigInteger number = new BigInteger(prime);
+	}
+}
--- code_pratice_ws/pratice_code/src/projecteuler/set1/test/p710001ThPrimeTest.java
+++ code_pratice_ws/pratice_code/src/projecteuler/set1/test/p710001ThPrimeTest.java
@@ -38,11 +38,11 @@ public class p710001ThPrimeTest {
     */ 
     @Test
     public void testGetPrime() throws Exception {
-		System.out.println("6th prime is " + testClass.getPrime(6));
+//		System.out.println("6th prime is " + testClass.getPrime(6));
 
-		System.out.println("10001st prime is " + testClass.getPrime(10001));
+//		System.out.println("10001st prime is " + testClass.getPrime(10001));
 
-		System.out.println("1000000th prime is " + testClass.getPrime(100000));
+		System.out.println("1000000th prime is " + testClass.getPrime(10000000));
 //		System.out.println("1000000th prime is " + testClass.getPrime(1000000));
 	}
     
