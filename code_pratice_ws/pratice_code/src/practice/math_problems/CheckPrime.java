351b8bda44e69f9676e833a8f8cc88da253abd80
prembhaskal <pbhaskal@gmail.com>
2013-05-06 14:40:30 +0530

    generating previous number with some probability.

--- code_pratice_ws/pratice_code/src/practice/math_problems/CheckPrime.java
+++ code_pratice_ws/pratice_code/src/practice/math_problems/CheckPrime.java
@@ -4,7 +4,16 @@ import java.math.BigInteger;
 
 public class CheckPrime {
 
-	public boolean isPrime(long prime) {
-		BigInteger number = new BigInteger(prime);
+	public boolean isPrime(long prime, int certainty) {
+		certainty = Math.max(10,certainty);
+
+		BigInteger number = BigInteger.valueOf(prime);
+		boolean isPrime = number.isProbablePrime(certainty);
+		return isPrime;
 	}
+
+	public boolean isPrime(long prime) {
+		return isPrime(prime,10);
+	}
+
 }
