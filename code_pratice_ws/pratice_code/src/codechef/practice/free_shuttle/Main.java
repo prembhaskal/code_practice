2ec4e64e81a5304f7059be9fbaf62086e9c70b74
prembhaskal <pbhaskal@gmail.com>
2013-05-11 02:34:28 +0530

    codechef new practice problem

---
+++ code_pratice_ws/pratice_code/src/codechef/practice/free_shuttle/Main.java
@@ -0,0 +1,77 @@
+package codechef.practice.free_shuttle;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] s) {
+		try {
+			InputStream inputStream = System.in;
+			InputReader in = new InputReader(inputStream);
+			PrintWriter writer = new PrintWriter(System.out);
+
+			TaskA solution = new TaskA();
+			solution.solve(in,writer);
+
+			writer.close();
+			inputStream.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+/**
+ * try to solve this http://ww2.codechef.com/problems/SHUTTLE
+ * both with/without euler totient function or whatever crap it is.
+ */
+class TaskA {
+
+	public void solve(InputReader in, PrintWriter out) throws IOException {
+
+		int tests = in.nextInt();
+
+		for (int i=0;i<tests;i++) {
+
+		}
+
+	}
+
+
+}
+
+class InputReader {
+	public BufferedReader reader;
+	public StringTokenizer tokenizer;
+
+	public InputReader(InputStream stream) {
+		reader = new BufferedReader(new InputStreamReader(stream));
+		tokenizer = null;
+	}
+
+	public String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong() {
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble() {
+		return Double.parseDouble(next());
+	}
+
+}
