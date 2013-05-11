00743697526b686050530b48312deb04a6498104
prembhaskal <pbhaskal@gmail.com>
2013-05-12 02:27:12 +0530

    passes but pretty slow solution

--- code_pratice_ws/pratice_code/src/codechef/practice/free_shuttle/Main.java
+++ code_pratice_ws/pratice_code/src/codechef/practice/free_shuttle/Main.java
@@ -26,6 +26,8 @@ public class Main {
 /**
  * try to solve this http://ww2.codechef.com/problems/SHUTTLE
  * both with/without euler totient function or whatever crap it is.
+ *
+ * The editorial has a strange way of getting euler's totient. find out what is it.
  */
 class TaskA {
 
@@ -34,9 +36,77 @@ class TaskA {
 		int tests = in.nextInt();
 
 		for (int i=0;i<tests;i++) {
+			int N = in.nextInt();
 
+			int result = getNosWithGCD1(N);
+
+//			getEulersTotient(N);
+
+			out.println(result);
 		}
 
+	}
+
+	// this approach passes without problem.
+	private int getNosWithGCD1(int N) {
+		int count = 1;
+
+		for (int i=2;i<N;i++) {
+			int gcd = getGCD(i, N);
+
+			if (gcd==1)
+				count++;
+		}
+
+		return count;
+	}
+
+	// or get number k, where k<N so that gcd(k,N)=1
+	private int getEulersTotient(int N) {
+		int count = 1; // shuttle no. 1 will always reach
+
+		for (int num = 2; num < N; num++) {
+			boolean shareDivisor = false;
+
+			for (int i=2;i<=num/i;i++) {
+				while (num%i==0) {
+					if (N%i==0) {
+						shareDivisor = true;
+						break;
+					}
+
+					num /= i;
+				}
+			}
+
+			if (num > 1) {
+				if (N%num==0) {
+					shareDivisor = true;
+				}
+			}
+
+
+			if (!shareDivisor) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+
+	public int getGCD(int a, int b) {
+		int num =b;
+		int div = a;
+
+		while (num > div) {
+			int rem = num%div;
+			if (rem==0)
+				break;
+			num = div;
+			div = rem;
+		}
+
+		return div;
 	}
 
 
