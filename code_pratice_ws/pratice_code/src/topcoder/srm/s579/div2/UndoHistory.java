c40b218568de787bf0a321223072027f6a6d4e95
prembhaskal <pbhaskal@gmail.com>
2013-05-31 14:42:28 +0530

    started the code for the question.

---
+++ code_pratice_ws/pratice_code/src/topcoder/srm/s579/div2/UndoHistory.java
@@ -0,0 +1,35 @@
+package topcoder.srm.s579.div2;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UndoHistory {
+
+	private Set<String>  undoHistory = new HashSet<String>();
+
+	public int minPresses(String[] lines) {
+		int minPresses = 0;
+
+		addToUndoHistory("");
+
+		for (String line : lines) {
+
+			int pressesForWord = 0;
+			String str = "";
+			char[] lineArr  = line.toCharArray();
+
+			for (int i = 0; i < lineArr.length; i++) {
+				str = str + lineArr[i];
+
+				
+			}
+		}
+
+		return minPresses;
+	}
+
+
+	public void addToUndoHistory(String str) {
+		undoHistory.add(str);
+	}
+}
