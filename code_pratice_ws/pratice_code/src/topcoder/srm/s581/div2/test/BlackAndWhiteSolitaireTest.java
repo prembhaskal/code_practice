3af5468afa3825c1f621c84f01323f90b4a1579e
prembhaskal <pbhaskal@gmail.com>
2013-06-04 11:07:09 +0530

    srm 581 q1

---
+++ code_pratice_ws/pratice_code/src/topcoder/srm/s581/div2/test/BlackAndWhiteSolitaireTest.java
@@ -0,0 +1,51 @@
+package topcoder.srm.s581.div2.test; 
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import static org.junit.Assert.*;
+import topcoder.srm.s581.div2.*;  
+
+/** 
+* BlackAndWhiteSolitaire Tester. 
+* 
+* @author <Premkumar Bhaskal> 
+* @since <pre>Jun 4, 2013</pre> 
+* @version 1.0 
+*/ 
+public class BlackAndWhiteSolitaireTest { 
+    
+    BlackAndWhiteSolitaire testClass = new BlackAndWhiteSolitaire();
+    long starttime;
+    
+    @Before
+    public void before() throws Exception {
+          starttime = System.nanoTime(); 
+    } 
+    
+    @After
+    public void after() throws Exception {
+		long now = System.nanoTime();
+		System.out.println("elapsed time " + (now-starttime)/1000000 + "milli secs");
+    } 
+    
+        /** 
+    * 
+    * Method: minimumTurns(String cardFront) 
+    * 
+    */ 
+    @Test
+    public void testMinimumTurns() throws Exception { 
+        String cardFront;
+
+		cardFront = "BBBW";
+		System.out.println("min flips are " + testClass.minimumTurns(cardFront));
+
+		cardFront = "WBWBW";
+		System.out.println("min flips are " + testClass.minimumTurns(cardFront));
+    } 
+    
+        
+} 
