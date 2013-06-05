d715de842ab32e1e2e15ac8f0dc7a22ad4ffb8f0
prembhaskal <pbhaskal@gmail.com>
2013-06-06 00:49:51 +0530

    max sub array problem.

---
+++ code_pratice_ws/pratice_code/src/algorithm/chap4/MaxSubArrayProblem.java
@@ -0,0 +1,64 @@
+package algorithm.chap4;
+
+public class MaxSubArrayProblem {
+
+	// input --> array storing the difference from previous num
+	public void getMaxSubArrayBruteForce(int[] num) {
+
+		int start = -1;
+		int end = -1;
+
+		int best = Integer.MIN_VALUE;
+
+		for (int i = 0; i < num.length; i++) {
+			int sum = 0;
+			for (int j = i; j < num.length; j++) {
+				sum += num[j];
+				if (sum > best) {
+					best = sum;
+					start = i;
+					end = j;
+				}
+			}
+		}
+
+		System.out.println("max value is " + best);
+		System.out.println("start is " + start + " and end is " + end);
+	}
+
+	public void getMaxSubArrayLinear(int[] num) {
+		int start = -1;
+		int end = -1;
+		int best = Integer.MIN_VALUE;
+
+		// get first the max sub array for 1 to size.
+		int sum = 0;
+		start = 0;
+		for (int i = 0; i < num.length; i++) {
+			sum += num[i];
+
+			if (sum > best) {
+				best = sum;
+				end = i;
+			}
+		}
+
+		sum = best;
+		for (int i=end+1;i<num.length;i++) {
+			for (int j=start;j<i;j++) {
+				sum = sum - num[j];
+
+				if (sum > best) {
+					start = j+1;
+					best = sum;
+					end = i;
+				}
+			}
+
+			sum = sum + num[i];
+		}
+
+		System.out.println("max value is " + best);
+		System.out.println("start is " + start + " and end is " + end);
+	}
+}
---
+++ code_pratice_ws/pratice_code/src/algorithm/chap4/test/MaxSubArrayProblemTest.java
@@ -0,0 +1,75 @@
+package algorithm.chap4.test; 
+
+import algorithm.UtilitiesClass;
+import java.util.Random;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+import algorithm.chap4.*;
+
+/** 
+* MaxSubArrayProblem Tester. 
+* 
+* @author <Premkumar Bhaskal> 
+* @since <pre>Jun 5, 2013</pre> 
+* @version 1.0 
+*/ 
+public class MaxSubArrayProblemTest { 
+    
+    MaxSubArrayProblem testClass = new MaxSubArrayProblem();
+    long starttime;
+    
+    @Before
+    public void before() throws Exception {
+          starttime = System.nanoTime(); 
+    } 
+    
+    @After
+    public void after() throws Exception {
+		long now = System.nanoTime();
+		System.out.println("elapsed time " + (now-starttime)/1000000 + "milli secs");
+    } 
+    
+        /** 
+    * 
+    * Method: getMaxSubArrayBruteForce(int[] num) 
+    * 
+    */ 
+    @Test
+    public void testGetMaxSubArrayBruteForce() throws Exception { 
+
+//		int[] num = getRandomArray(10,10);
+		int[] num = new int[]{0, -3, 8 ,0 ,-9, -9, 8, 6, 3, -5 };
+		System.out.println("************** Array is ***************");
+		UtilitiesClass.printArray(num);
+
+		testClass.getMaxSubArrayBruteForce(num);
+
+		testClass.getMaxSubArrayLinear(num);
+
+//		num = getRandomArray(10000,100);
+		System.out.println("************** Array is ***************");
+//		UtilitiesClass.printArray(num);
+
+//		testClass.getMaxSubArrayBruteForce(num);
+    } 
+
+	// array has both +ve and -ve numbers in the range.
+    private int[] getRandomArray(int size, int range) {
+		int[] num = new int[size];
+
+		Random random = new Random();
+		Random evenOrOdd = new Random();
+
+		for (int i=0;i<size;i++) {
+			int rnd = random.nextInt(range);
+
+			if (evenOrOdd.nextInt(2)==0)
+				rnd = rnd * -1;
+
+			num[i] = rnd;
+		}
+
+		return num;
+	}
+} 
