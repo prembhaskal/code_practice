a93541cf39f83a95398a27041fdec1bb8d75db97
prembhaskal <pbhaskal@gmail.com>
2013-07-07 23:52:22 +0530

    Accepted in first attempt....YES YES YES

--- code_pratice_ws/pratice_code/src/codechef/july13/galactik/Main.java
+++ code_pratice_ws/pratice_code/src/codechef/july13/galactik/Main.java
@@ -3,6 +3,7 @@ package codechef.july13.galactik;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -47,20 +48,106 @@ class TaskA {
 			neighbours[i][1] = in.nextInt();
 		}
 
-		tax = new int[noOfPlanets];
-		for (int i = 0; i <noOfPlanets; i++) {
+		tax = new int[noOfPlanets+1];
+		for (int i = 1; i <noOfPlanets+1; i++) {
 			tax[i] = in.nextInt();
 		}
 
-	}
-
-	private int getMinAmount() {
-		throw new RuntimeException();
+		long minTax = getMinAmount();
+		out.println(minTax);
 	}
 
 	private Graph graph;
+	private boolean []isVisited;
 
+	private long getMinAmount() {
+		formGraph();
+		isVisited = new boolean[noOfPlanets+1];
 
+		boolean notPossible = false;
+		List<Integer> sccMinVals= new ArrayList<Integer>();
+
+		for (int node=1;node<noOfPlanets+1;node++) {
+			if (!isVisited[node]) {
+				int sccTax = traverseGraphAndGetMin(node);
+				if (sccTax < 0) {
+					notPossible = true;
+					// TODO optimize here... can break here if SCC.size() > 0
+				}
+				sccMinVals.add(sccTax);
+			}
+		}
+
+		if (sccMinVals.size()==1)
+			return 0; // everyone is connected. no need to pay.
+
+		if (notPossible) {
+			return -1;
+		} else {
+			return getTaxToPay(sccMinVals);
+		}
+	}
+
+	private long getTaxToPay(List<Integer> taxVals) {
+		int size = taxVals.size();
+		int min = Integer.MAX_VALUE;
+		long total = 0;
+		for (Integer tax : taxVals) {
+			total += tax;
+			min = Math.min(tax, min);
+		}
+
+		long tax = total + (size-2)*min; // all other SCC connect to the SCC with min value.
+		return tax;
+	}
+
+	private int traverseGraphAndGetMin(Integer node) {
+		int min = -1;
+		isVisited[node] = true;
+		Stack<Integer> stack = new Stack<Integer>();
+		stack.push(node);
+
+		while (!stack.isEmpty()) {
+			Integer fromNode = stack.pop();
+			min = getMinTax(tax[fromNode], min);
+			List<Integer> neighbours = graph.adjacencyList[fromNode];
+			if (neighbours!= null) {
+				for (Integer neighbour : neighbours) {
+					if (!isVisited[neighbour]) {
+						isVisited[neighbour] = true;
+						stack.push(neighbour);
+					}
+				}
+
+			}
+		}
+
+		return min;
+	}
+
+	private int getMinTax(int a, int b) {
+		int min = Math.min(a,b);
+		int max = Math.max(a,b);
+
+		if (min >= 0) // if both are positive
+			return min;
+		else if (max >= 0) // if one is negative
+			return max;
+		else // if both are negative,
+			return -1;
+	}
+
+	private void formGraph() {
+		List<Integer>[] adjacencyList = new ArrayList[noOfPlanets+1];
+		graph = new Graph(adjacencyList);
+
+		for (int i = 0; i < pairs; i++) {
+			Integer fromNode = neighbours[i][0];
+			Integer toNode = neighbours[i][1];
+			graph.addNeighbour(fromNode, toNode);
+			graph.addNeighbour(toNode, fromNode);
+		}
+	}
 
 }
 
