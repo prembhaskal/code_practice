88706a54ace4ea0c89edf7336a9fa205b1207f4d
prembhaskal <pbhaskal@gmail.com>
2013-08-04 22:51:46 +0530

    solve this TODO.

---
+++ code_pratice_ws/pratice_code/src/codechef/august13/chmod/Main.java
@@ -0,0 +1,81 @@
+package codechef.august13.chmod;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] s) {
+		try {
+			InputStream inputStream = System.in;
+			InputReader in = new InputReader(inputStream);
+			PrintWriter writer = new PrintWriter(System.out);
+
+			TaskA solution = new TaskA();
+			solution.solve(in,writer);
+
+			writer.close();
+			inputStream.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class TaskA {
+
+	int elements;
+	int[] nums;
+	BigInteger[] prodArray;
+
+	public void solve(InputReader in, PrintWriter out) throws IOException {
+
+		elements = in.nextInt();
+		nums = new int[elements];
+
+		for (int i = 0; i < elements; i++) {
+			nums[i] = in.nextInt();
+		}
+
+		int[][] elems = new int[100000][100];
+	}
+
+
+}
+
+class InputReader {
+	public BufferedReader reader;
+	public StringTokenizer tokenizer;
+
+	public InputReader(InputStream stream) {
+		reader = new BufferedReader(new InputStreamReader(stream));
+		tokenizer = null;
+	}
+
+	public String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong() {
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble() {
+		return Double.parseDouble(next());
+	}
+
+}
