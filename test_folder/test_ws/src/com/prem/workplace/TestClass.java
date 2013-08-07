65eb437c2032ec5892b4aecfaa894fd0f0d50f1a
prembhaskal <pbhaskal@gmail.com>
2013-08-07 17:18:34 +0530

    minor things.

--- test_folder/test_ws/src/com/prem/workplace/TestClass.java
+++ test_folder/test_ws/src/com/prem/workplace/TestClass.java
@@ -14,7 +14,8 @@ public class TestClass {
 
 	public static void main(String[] s) {
 		TestClass testClass = new TestClass();
-		testClass.generateData();
+//		testClass.generateData();
+		testClass.gen();
 	}
 
 	// data generator.
@@ -40,7 +41,31 @@ public class TestClass {
 			if (out != null)
 				out.close();
 		}
+	}
 
+	private void gen() {
+		File file = new File("D://testData.txt");
+		PrintWriter out = null;
+		int million = 1000000;
+		int val;
+		try {
+			out = new PrintWriter(file);
 
+			String line;
+			for (int i=1;i<=30000000;i++) {
+				val = i % million;
+				line = "";
+				line += val + "," ;
+				line += "03/22/2013 00:00:00,";
+				line += "3.14";
+				out.println(line);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (out != null)
+				out.close();
+		}
 	}
 }
