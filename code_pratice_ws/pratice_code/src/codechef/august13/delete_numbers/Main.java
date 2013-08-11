1dc84d7d203c1e1b234cd3204c017c6325c64b87
prembhaskal <pbhaskal@gmail.com>
2013-08-12 00:14:53 +0530

    stuck at this question :( :(.

--- code_pratice_ws/pratice_code/src/codechef/august13/delete_numbers/Main.java
+++ code_pratice_ws/pratice_code/src/codechef/august13/delete_numbers/Main.java
@@ -1,8 +1,7 @@
 package codechef.august13.delete_numbers;
 
 import java.io.*;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] s) {
@@ -26,6 +25,8 @@ public class Main {
 
 class TaskA {
 
+	int maxRange = 100000;
+
 	int size;
 	int[] nums;
 
@@ -42,7 +43,8 @@ class TaskA {
 		}
 
 		// find the moves
-		findMinMovesNaive();
+//		findMinMovesNaive();
+		findMinMoves();
 
 		// print the moves;
 		out.println(moves);
@@ -54,7 +56,7 @@ class TaskA {
 
 	}
 
-	// just keep deleting the numbers from end. THIS IS STUPID METHOD TO DO. but still fetches you marks. ;) ;)
+	// just keep deleting the numbers from end. THIS IS STUPID WAY TO DO. but still fetches you marks. ;) ;)
 	private void findMinMovesNaive() {
 		moves = size;
 
@@ -73,6 +75,73 @@ class TaskA {
 		boolean[] numDeletedArray = new boolean[size];
 
 		// map of indexes for each number
+		Map<Integer,List<Integer>> numVsIndexes = new HashMap<>();
+
+		// fill this map by traversing in reverse.
+		for (int idx = size-1; idx >=0; idx--) {
+			List<Integer> indexes = numVsIndexes.get(nums[idx]);
+			if (indexes==null) {
+				indexes = new LinkedList<>(); // linked list so that we can remove efficiently.
+				numVsIndexes.put(nums[idx], indexes);
+			}
+			indexes.add(idx+1);
+		}
+
+		// start finding the moves, start from the last number
+		moves = 0;
+		varray = new int[maxRange]; // max range so that we avoid arraylist.
+		tarray = new int[maxRange];
+
+		for (int i = size-1; i >= 0; i--) {
+
+			if (numDeletedArray[i]) {
+				continue;
+			}
+
+			int num = nums[i];
+			int[] v_and_t = getVAndTArray(numVsIndexes, num);
+
+			int[] newIdxs = getNewIndexesAfterRemovingNumbers(v_and_t[0], v_and_t[1]);
+			// store this in max range
+
+			varray[moves] = newIdxs[0];
+			tarray[moves] = newIdxs[1];
+
+			// mark these num
+			numDeletedArray[v_and_t[0]-1] = true;
+			numDeletedArray[v_and_t[1]-1] = true;
+
+			moves++;
+		}
+	}
+
+	// TODO implement this method properly, indexes change once we remove elements from array.
+	// TODO implement someway which runs faster than O(n).
+	private int[] getNewIndexesAfterRemovingNumbers(int lowIdx, int highIdx) {
+		int[] idxs = new int[2];
+		idxs[0] = lowIdx;
+		idxs[1] = highIdx;
+		return idxs;
+	}
+
+	private int[] getVAndTArray(Map<Integer,List<Integer>> numVsIndexes, int num) {
+		int[] v_and_t = new int[2];
+
+		List<Integer> indexes = numVsIndexes.get(num);
+
+		if (indexes.size()==1) {
+			v_and_t[1] = v_and_t[0] = indexes.get(0);
+			return v_and_t;
+		} else {
+			Iterator<Integer> iterator = indexes.iterator();
+			// bigger index will be second
+			v_and_t[1] = iterator.next();
+			iterator.remove();
+			v_and_t[0] = iterator.next();
+			iterator.remove();
+
+			return v_and_t;
+		}
 	}
 
 
