f21ca5d6b1948f43f9dcdf67e3312a5335884c9b
prembhaskal <pbhaskal@gmail.com>
2013-08-18 12:37:47 +0530

    segment delete tree with just recursive nodes.

---
+++ code_pratice_ws/pratice_code/src/practice/algorithm/SegmentDeleteTree.java
@@ -0,0 +1,88 @@
+package practice.algorithm;
+
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+/**
+ * segment tree.
+ * following operations are supported for an array.
+ * deleting an element at any index in O(log(n)) time.
+ * getting new index for an old index in O(log(n)).. NOTE a normal array would take O(n) for this operation.
+ * in normal array, after deletion we would need to shift the whole array left...so O(n)
+ *
+ * In short this tree supports multiple deletes and querying in O(log(n)) time.
+ *
+ * since we are only interested in the indices, this tree will be based on only the size of the array irrespective
+ * of the actual elements in the array.
+ *
+ * 0 based index.
+ */
+public class SegmentDeleteTree {
+
+	int size;
+	Node rootNode;
+
+	public SegmentDeleteTree(int size) {
+		this.size = size;
+		if (size==0)
+			return;
+		formTree();
+	}
+
+	private void formTree() {
+		// form the top-node.
+
+		rootNode = new Node(0, size-1);
+		rootNode.elementsToLeft = (rootNode.high - rootNode.low + 1)/2;
+		rootNode.totalElementsBelow = (rootNode.high - rootNode.low + 1);
+
+		formSubTree(rootNode);
+	}
+
+	private void formSubTree(Node parentNode) {
+		if (parentNode.low >= parentNode.high) // STOP AT LEAF
+			return;
+
+		int L = parentNode.low;
+		int H = parentNode.high;
+		int mid = L + (H-L)/2;
+
+		Node leftNode = new Node(L, mid);
+		formSubTree(leftNode);
+		Node rightNode = new Node(mid + 1, H);
+		formSubTree(rightNode);
+
+		parentNode.leftNode = leftNode;
+		parentNode.rightNode = rightNode;
+	}
+
+	public void printSegmentTree(PrintStream out) {
+		printNode(this.rootNode, out);
+	}
+
+	private void printNode(Node node, PrintStream out) {
+		if (node==null)
+			return;
+
+		// print root node.
+		System.out.println("-->" + node.low + " -- " + node.high + "<--");
+		printNode(node.leftNode, out);
+		printNode(node.rightNode, out);
+	}
+}
+
+class Node {
+	int low;
+	int high;
+
+	Node leftNode;
+	Node rightNode;
+
+	int elementsToLeft;
+	int totalElementsBelow;
+
+	public Node(int low, int high) {
+		this.low = low;
+		this.high = high;
+	}
+}
