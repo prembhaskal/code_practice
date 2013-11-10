fd9aa65f31446cc37490f229266b3e7522eb038e
prembhaskal <pbhaskal@gmail.com>
2013-11-11 01:18:02 +0530

    factorial question...i almost solved it by myself ;)

---
+++ code_pratice_ws/pratice_code/src/spoj/set1/p11factorial/Main.java
@@ -0,0 +1,85 @@
+package spoj.set1.p11factorial;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] s) {
+		try {
+			InputStream inputStream = System.in;
+			InputReader in = new InputReader(inputStream);
+			PrintWriter writer = new PrintWriter(System.out);
+
+			TaskA solution = new TaskA();
+			solution.solve(in,writer);
+
+			writer.close();
+			inputStream.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class TaskA {
+
+	public void solve(InputReader in, PrintWriter out) throws IOException {
+
+		int tests = in.nextInt();
+
+		for (int i=0;i<tests;i++) {
+			int num = in.nextInt();
+			int zeroes = getTrailingZeroesInFactorial(num);
+			out.println(zeroes);
+		}
+
+	}
+
+	private int getTrailingZeroesInFactorial(int num) {
+		int zeroes = 0;
+		while (num > 0) {
+			zeroes += num/5;
+			num /= 5;
+		}
+
+		return zeroes;
+	}
+
+
+}
+
+class InputReader {
+	public BufferedReader reader;
+	public StringTokenizer tokenizer;
+
+	public InputReader(InputStream stream) {
+		reader = new BufferedReader(new InputStreamReader(stream));
+		tokenizer = null;
+	}
+
+	public String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong() {
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble() {
+		return Double.parseDouble(next());
+	}
+
+}
