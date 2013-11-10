a6313ae1fea4034c5413c62a700a742003b35956
prembhaskal <pbhaskal@gmail.com>
2013-11-10 23:25:39 +0530

    enormous input test.....very slow.

---
+++ code_pratice_ws/pratice_code/src/spoj/set1/pnintest/Main.java
@@ -0,0 +1,77 @@
+package spoj.set1.pnintest;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] s) {
+		try {
+			InputStream inputStream = System.in;
+			InputReader in = new InputReader(inputStream);
+			PrintWriter writer = new PrintWriter(System.out);
+
+			TaskA solution = new TaskA();
+			solution.solve(in,writer);
+
+			writer.close();
+			inputStream.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
+
+class TaskA {
+
+	public void solve(InputReader in, PrintWriter out) throws IOException {
+		int n = in.nextInt();
+		int k = in.nextInt();
+
+		int count = 0;
+		for (int i = 0; i < n; i++) {
+			int num = in.nextInt();
+			if (num % k == 0)
+				count++;
+		}
+
+		out.println(count);
+	}
+
+
+}
+
+class InputReader {
+	public BufferedReader reader;
+	public StringTokenizer tokenizer;
+
+	public InputReader(InputStream stream) {
+		reader = new BufferedReader(new InputStreamReader(stream));
+		tokenizer = null;
+	}
+
+	public String next() {
+		while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+			try {
+				tokenizer = new StringTokenizer(reader.readLine());
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return tokenizer.nextToken();
+	}
+
+	public int nextInt() {
+		return Integer.parseInt(next());
+	}
+
+	public long nextLong() {
+		return Long.parseLong(next());
+	}
+
+	public double nextDouble() {
+		return Double.parseDouble(next());
+	}
+
+}
