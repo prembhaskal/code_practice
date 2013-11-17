9edd19e360cc2a54e2da5b1967d1c3b8465af07c
prembhaskal <pbhaskal@gmail.com>
2013-11-17 17:05:15 +0530

    not very accurate code.

--- code_pratice_ws/pratice_code/src/projecteuler/set6/P51PrimDigReplace.java
+++ code_pratice_ws/pratice_code/src/projecteuler/set6/P51PrimDigReplace.java
@@ -16,61 +16,114 @@ public class P51PrimDigReplace {
 
 	private void searchForPairs(int digits) {
 		// digit 0 will not be searched as it has only odd nos.
-		int maxi = -1;
-		int maxj = -1;
+		int[] maxReplaceDig = null;
 		int maxScore = -1;
 		int maxRemNum = -1;
-		for (int i = 1; i < digits; i++) {
-			for (int j = i + 1; j < digits; j++) {
-				MaxRemNumMaxScore mrScore = getScore(i, j);
-				if (mrScore.maxScore > maxScore) {
-					maxScore = mrScore.maxScore;
-					maxi = i;
-					maxj = j;
-					maxRemNum = mrScore.remNum;
-				}
+
+		// generate all mask for 2,3 digits replacing.
+		List<int[]> replaceDigits = get3ReplaceDigits(digits);
+//		List<int[]> replaceDigits = get2ReplaceDigits(digits);
+
+		// store the maxCount using this mask... replaceDig.
+		for (int[] replaceDig : replaceDigits) {
+			MaxRemNumMaxScore mrScore = getScore(replaceDig);
+			if (mrScore.maxScore > maxScore) {
+				maxScore = mrScore.maxScore;
+				maxRemNum = mrScore.remNum;
+				maxReplaceDig = replaceDig;
 			}
 		}
 
-		System.out.println("max was for digits " + maxi + " and " + maxj + " with a max score of " + maxScore
-							+ " with rem num as " + maxRemNum);
+		System.out.print("max replace digs are ");
+		for (int i = 0; i < maxReplaceDig.length; i++) {
+			System.out.print(maxReplaceDig[i] + " ");
+		}
+		System.out.println("");
+		System.out.println("with a max score of " + maxScore + " with rem num as " + maxRemNum);
 	}
 
 	private Map<Integer, Integer> positionsVsRemNum = new HashMap<>();
 
-	private MaxRemNumMaxScore getScore(int pos1, int pos2) {
+	// apply the max pos, and check the remaining of the number. store the max count.
+	// this is done using map<remNumber, countofThat>.
+	private MaxRemNumMaxScore getScore(int[] pos) {
 		int maxScore = -1;
 		int maxForRemNum = -1;
 		Map<Integer, Integer> remNumVsCount = new HashMap<>();
 		for (int[] prime : primeArrays) {
-			if (prime[pos1] == prime[pos2]) {
-				int remNum = getRemNum(prime, pos1, pos2);
-				// put in the map
-				Integer count = remNumVsCount.get(remNum);
-				if (count == null) {
-					count = 0;
-				}
-				count += 1;
-				remNumVsCount.put(remNum, count);
-				if (count > maxScore) {
-					maxScore = count;
-					maxForRemNum = remNum;
+			boolean matching = true;
+			for (int i = 1; i < pos.length; i++) {
+				if (prime[pos[i-1]] != prime[pos[i]]) {
+					matching = false;
+					break;
 				}
 			}
+
+			if (!matching)
+				continue;
+
+			int remNum = getRemNum(prime, pos);
+			// put in the map
+			Integer count = remNumVsCount.get(remNum);
+			if (count == null) {
+				count = 0;
+			}
+			count += 1;
+			remNumVsCount.put(remNum, count);
+			if (count > maxScore) {
+				maxScore = count;
+				maxForRemNum = remNum;
+			}
+
 
 		}
 
 		return new MaxRemNumMaxScore(maxForRemNum, maxScore);
 	}
 
-	private int getRemNum(int[] prime, int p1, int p2) {
+	// using the prime and replace digit, find the remaining num, so that we can easily use it in map.
+	// this is analogous to NUMBER & BIT_MASK operation....
+	private int getRemNum(int[] prime, int[] pos) {
 		int num = 0;
-		for (int i = prime.length -1; i >= 0; i--) {
-			if (i == p1 || i == p2)
+		boolean [] notUseDigit = new boolean[prime.length];
+		for (int i = 0; i < pos.length; i++) {
+			notUseDigit[pos[i]] = true;
+		}
+
+
+		for (int i = prime.length - 1; i >= 0; i--) {
+			if (notUseDigit[i])
 				continue;
 			num = num * 10 + prime[i];
 		}
 		return num;
+	}
+
+	// TODO write a fast, simple recursive function for it.
+	private List<int[]> get3ReplaceDigits(int totalDigits) {
+		List<int[]> replaceDigits = new ArrayList<>();
+		for (int i = 1; i < totalDigits; i++) {
+			for (int j = i + 1; j < totalDigits; j++) {
+				for (int k = j + 1; k < totalDigits; k++) {
+					int[] digits = new int[]{i, j, k};
+					replaceDigits.add(digits);
+				}
+			}
+		}
+
+		return replaceDigits;
+	}
+
+	private List<int[]> get2ReplaceDigits(int totalDigits) {
+		List<int[]> replaceDigits = new ArrayList<>();
+		for (int i = 1; i < totalDigits; i++) {
+			for (int j = i + 1; j < totalDigits; j++) {
+				int[] digits = new int[]{i, j,};
+				replaceDigits.add(digits);
+			}
+		}
+
+		return replaceDigits;
 	}
 
 	private void initPrimes(int range) {
@@ -90,6 +143,7 @@ public class P51PrimDigReplace {
 		}
 	}
 
+	// STORING primes as arrays so that we easily mask them.
 	private void initPrimeAsArrays(int digits) {
 		int low = 1;
 		for (int i = 0; i < digits - 1; i++) {
