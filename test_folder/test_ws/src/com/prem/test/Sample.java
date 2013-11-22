5b9630eaba0a9ca07a49466b3335ef7f144cff1d
prembhaskal <pbhaskal@gmail.com>
2013-11-22 18:45:43 +0530

    minor things

---
+++ test_folder/test_ws/src/com/prem/test/Sample.java
@@ -0,0 +1,19 @@
+package com.prem.test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Sample {
+
+//	public <T> void setArrayList(ArrayList<T> arrayList, T t) {
+//
+//	}
+
+	public <T> void setArrayList(ArrayList arrayList, Object obj) {
+
+	}
+//
+//	public <T extends HashMap<K,V>> void setArrayList(ArrayList arrayList){
+//
+//	}
+}
--- test_folder/test_ws/src/com/prem/test/TestClass.java
+++ test_folder/test_ws/src/com/prem/test/TestClass.java
@@ -1,5 +1,8 @@
 package com.prem.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * User: premkumar.bhaskal
  * Date: 3/1/13
@@ -9,7 +12,14 @@ public class TestClass {
 
 
 	public static void main(String[] s) {
-		testDivision();
+//		testDivision();
+//
+//		String[] strArray1 = new String[]{"test", "test1"};
+//		readVarargs(strArray1);
+//		readVarargs((Object)strArray1);
+//		String[] strArray2 = new String[]{"test2", "test3"};
+//		readVarargs(strArray1, strArray2);
+		testIntegerMap();
 	}
 
 
@@ -30,6 +40,37 @@ public class TestClass {
 		}
 
 		System.out.println("done");
+	}
+
+	private static Object[] objs;
+	private static void readVarargs(Object... arg) {
+		objs = arg;
+
+		System.out.println("size of varargs is " + objs.length);
+		for (int i = 0; i < objs.length; i++) {
+			System.out.println(objs[i]);
+		}
+	}
+
+	// this means Integer is immutable., if countMap.put is skipped the value will remain as 0.
+	private static void testIntegerMap() {
+		Map<String, Integer> countMap = new HashMap<>();
+		String[] strs = new String[]{"1", "2", "1"};
+
+		for (String str : strs) {
+			Integer count = countMap.get(str);
+			if (count == null) {
+				count = 0;
+				countMap.put(str, count);
+			}
+
+			count = count + 1;
+			countMap.put(str, count);
+		}
+
+		for (String str : strs) {
+			System.out.println(countMap.get(str));
+		}
 	}
 
 }
