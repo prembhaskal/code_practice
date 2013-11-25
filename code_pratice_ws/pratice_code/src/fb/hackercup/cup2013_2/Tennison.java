f243a16a077b76718410e30bd23a8630cd83daf4
prembhaskal <pbhaskal@gmail.com>
2013-11-25 22:13:09 +0530

    should have done this.....

--- code_pratice_ws/pratice_code/src/fb/hackercup/cup2013_2/Tennison.java
+++ code_pratice_ws/pratice_code/src/fb/hackercup/cup2013_2/Tennison.java
@@ -31,6 +31,10 @@ public class Tennison {
 		return getProbability(1, 0, Pi, 1.0);
 	}
 
+	// TODO the brute force should be changed a little so that
+	// if we win,  the sun can remain the same.
+	//         OR, the sun can increase...
+	// similarly for losing.
 	private double getProbability(int set, int wins, double Pi, double probTillHere) {
 		if (wins == setsToWin)
 			return probTillHere;
@@ -38,35 +42,22 @@ public class Tennison {
 			return 0;
 
 		double probWinning = Ps * Pi + Pr * (1 - Pi);
-		// we win this set.
-		double P1 = getProbability(set + 1, wins + 1, Math.min(1.0, Pi + Pu * Pw), probTillHere * probWinning);
-		// we lose this set.
-		double P2 = getProbability(set + 1, wins, Math.max(0.0, Pi - Pd*Pl), probTillHere * (1-probWinning));
+		// win and more sun
+		double P1 = getProbability(set + 1, wins + 1, Math.min(1.0, Pi + Pu), probTillHere * probWinning * Pw);
+		// win and same sun
+		double P2 = getProbability(set + 1, wins + 1, Pi, probTillHere * probWinning * (1 - Pw));
+		// lose and lesser sun
+		double P3 = getProbability(set + 1, wins, Math.max(0.0, Pi - Pd), probTillHere * (1-probWinning) * Pl);
+		// lose and same sun
+		double P4 = getProbability(set + 1, wins, Pi, probTillHere * (1 - probWinning) * (1 - Pl));
 
-		return P1 + P2;
+		return P1 + P2 + P3 + P4;
 	}
 
-//	private double getProbabilityDP() {
-//		int maxSetPossible = setsToWin*2;
-//		double [][] prodArray = new double[maxSetPossible][maxSetPossible];
-//
-//		// we win in first set
-//		prodArray[1][1] = prodHere(Ps, 0, 0);
-//		// we lose in first set
-//		prodArray[0][1] = 1 - prodArray[1][1];
-//
-//		for (int i = 0; i <= setsToWin; i++) {
-//			for (int j = i + 1; j < maxSetPossible; j++) {
-//				prodArray[i][j] = prodHere(Ps, i, j - i) * prodArray[i-1][j-1];
-//				pro
-//			}
-//		}
-//	}
-//
-//	private double prodHere(double probSun, double inc, double dec) {
-//		double prod = Pr * probSun  + Ps * (1-probSun);
-//		prod += inc;
-//		prod -= dec;
-//		return prod;
-//	}
+	// solution using DP...
+	// the solution uses 3d array...similar to the method above which uses 3 variables to decide the
+	// the probability of the next set.
+
+	// one guy even used a map of double...some craziness...
+	// most people multiplied by 1000, to convert to integer....to apply DP..didn't get the logic.
 }
