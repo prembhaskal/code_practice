4bbde8afb5140402c3c16409b7d2d7075aaa0501
prembhaskal <pbhaskal@gmail.com>
2013-11-26 23:19:52 +0530

    simpler and faster way than using -1.

--- code_pratice_ws/pratice_code/src/projecteuler/set6/P53CombinatSelections.java
+++ code_pratice_ws/pratice_code/src/projecteuler/set6/P53CombinatSelections.java
@@ -6,9 +6,6 @@ public class P53CombinatSelections {
 
 		long[][] ncrValues = new long[101][101];
 
-		// we shall represent nos > million by -1;
-		// total count of -1 will be the answer.
-
 		// fill the first no. with 1
 		for (int i = 0; i < ncrValues.length; i++) {
 			ncrValues[i][0] = 1;
@@ -19,16 +16,10 @@ public class P53CombinatSelections {
 		// start filling the pascal's triangle
 		for (int i = 1; i < ncrValues.length; i++) {
 			for (int j = 1; j <= i; j++) {
-				if (ncrValues[i-1][j-1] == -1 || ncrValues[i-1][j] == -1) { // number more than million would be set to -1 below
-					ncrValues[i][j] = -1;
+				ncrValues[i][j] = ncrValues[i-1][j-1] + ncrValues[i-1][j];
+				if (ncrValues[i][j] > 1000_000) {
+					ncrValues[i][j] = 1000_000; // set it to million so that it does not increase again.
 					count++;
-				}
-				else {
-					ncrValues[i][j] = ncrValues[i-1][j-1] + ncrValues[i-1][j];
-					if (ncrValues[i][j] > 1000_000) {
-						ncrValues[i][j] = -1;
-						count++;
-					}
 				}
 			}
 		}
