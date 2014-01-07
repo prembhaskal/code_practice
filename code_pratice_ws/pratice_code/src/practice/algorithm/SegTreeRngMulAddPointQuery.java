6e180f4c90ffe61c34ae2dbbf85edf4759305437
prembhaskal <pbhaskal@gmail.com>
2014-01-08 01:02:26 +0530

    Segment Tree. range multiplication. range addition.

---
+++ code_pratice_ws/pratice_code/src/practice/algorithm/SegTreeRngMulAddPointQuery.java
@@ -0,0 +1,123 @@
+package practice.algorithm;
+
+public class SegTreeRngMulAddPointQuery {
+
+	private int[] nums;
+	private int size;
+
+	private Node rootNode;
+
+	public SegTreeRngMulAddPointQuery(int[] nums) {
+		this.nums = nums;
+		size = nums.length;
+		init();
+	}
+
+	private void init() {
+		rootNode = new Node(0, size - 1);
+		addNode(rootNode);
+	}
+
+	private void addNode(Node parentNode) {
+		int low = parentNode.low;
+		int high = parentNode.high;
+
+		if (low == high) { // we reached leaf.
+			parentNode.value = nums[low];
+			return;
+		}
+
+		int mid = (low + high)/2;
+		parentNode.leftNode = new Node(low, mid);
+		parentNode.rightNode = new Node(mid + 1, high);
+
+		addNode(parentNode.leftNode);
+		addNode(parentNode.rightNode);
+	}
+
+
+	// range multiply
+	public void rangeMultiply(int M, int low, int high) {
+		rngMultiplyAdd(rootNode, M, 0, low, high);
+	}
+
+	// range add
+	public void rangeAdd(int A, int low, int high) {
+		rngMultiplyAdd(rootNode, 1, A, low, high);
+	}
+
+	private void rngMultiplyAdd(Node parentNode, int M, int A, int i, int j) {
+		int low = parentNode.low;
+		int high = parentNode.high;
+
+		if (low > j || high < i) // non overlapping.
+			return;
+
+		if (low >= i && high <= j) {
+			parentNode.updateNode(M, A);
+			return;
+		}
+
+		parentNode.splitNode();
+
+		rngMultiplyAdd(parentNode.leftNode, M, A, i, j);
+		rngMultiplyAdd(parentNode.rightNode, M, A, i, j);
+	}
+
+	public int pointQuery(int point) {
+		return pointQuery(rootNode, point);
+	}
+
+	private int pointQuery(Node parentNode, int point) {
+		if (parentNode.low == parentNode.high && parentNode.low == point)
+			return parentNode.getValue();
+
+		parentNode.splitNode();
+
+		Node leftNode = parentNode.leftNode;
+		Node rightNode = parentNode.rightNode;
+
+		if (leftNode.low <= point && leftNode.high >= point) {
+			return pointQuery(leftNode, point);
+		} else {
+			return pointQuery(rightNode, point);
+		}
+	}
+
+	private class Node {
+		int low;
+		int high;
+
+		int value; // this is only for leaf.
+
+		Node leftNode;
+		Node rightNode;
+
+		int mul = 1;
+		int add = 0;
+
+		public Node(int low, int high) {
+			this.low = low;
+			this.high = high;
+		}
+
+		// push changes in a recursive nature
+		public void updateNode(int M, int A) {
+			mul = mul * M;
+			add = add*M + A;
+		}
+
+		public int getValue() {
+			return value * mul + add;
+		}
+
+		public void splitNode() {
+			leftNode.updateNode(mul, add);
+			rightNode.updateNode(mul, add);
+			mul = 1;
+			add = 0;
+		}
+
+
+	}
+}
---
+++ code_pratice_ws/pratice_code/src/practice/algorithm/test/SegTreeRngMulAddPointQueryTest.java
@@ -0,0 +1,83 @@
+package practice.algorithm.test; 
+
+import java.util.Scanner;
+import java.io.PrintWriter;
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+import static org.junit.Assert.*;
+import practice.algorithm.*;  
+
+/** 
+* SegTreeRngMulAddPointQuery Tester. 
+* 
+* @author <Premkumar Bhaskal> 
+* @since <pre>Jan 8, 2014</pre> 
+* @version 1.0 
+*/ 
+public class SegTreeRngMulAddPointQueryTest { 
+    
+    SegTreeRngMulAddPointQuery testClass;
+    long starttime;
+    
+    @Before
+    public void before() throws Exception {
+          starttime = System.nanoTime(); 
+    } 
+    
+    @After
+    public void after() throws Exception {
+		long now = System.nanoTime();
+		System.out.println("elapsed time " + (now-starttime)/1000000 + "milli secs");
+    }
+
+
+	private int[] nums;
+
+	@Before
+	public void init() {
+		nums = new int[]{2, 5, 0, 6, 9};
+		testClass = new SegTreeRngMulAddPointQuery(nums);
+	}
+
+	private void checkPointQuery() {
+		for (int i = 0; i < nums.length; i++) {
+			int exp = nums[i];
+			int act = testClass.pointQuery(i);
+
+			assertEquals("incorrect for index --> " + i, exp, act);
+		}
+	}
+
+	@Test
+	public void testTheTree() {
+		checkPointQuery();
+
+		rangeMultiply(0, 3, 3);
+		checkPointQuery();
+
+		rangeMultiply(1, 3, 2);
+		rangeAdd(2, 4, 7);
+		checkPointQuery();
+
+		rangeMultiply(3, 3, 2);
+		rangeAdd(1, 4, 7);
+		rangeAdd(2, 4, 9);
+		checkPointQuery();
+	}
+
+
+	private void rangeMultiply(int low, int high, int value) {
+		for (int i = low; i <= high; i++) {
+			nums[i] *= value;
+		}
+		testClass.rangeMultiply(value, low, high);
+	}
+
+	private void rangeAdd(int low, int high, int value) {
+		for (int i = low; i <= high; i++) {
+			nums[i] += value;
+		}
+		testClass.rangeAdd(value, low, high);
+	}
+} 
