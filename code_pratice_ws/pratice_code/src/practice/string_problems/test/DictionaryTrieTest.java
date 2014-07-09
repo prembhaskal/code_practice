9a0b7c651347370433835b5ba9b57999017561a3
prembhaskal <pbhaskal@gmail.com>
2014-07-09 13:34:08 +0530

    updated the dictionary and other minor things.

--- code_pratice_ws/pratice_code/src/practice/string_problems/test/DictionaryTrieTest.java
+++ code_pratice_ws/pratice_code/src/practice/string_problems/test/DictionaryTrieTest.java
@@ -3,10 +3,10 @@ package practice.string_problems.test;
 import common.util.InputReader;
 import java.io.File;
 import java.io.InputStream;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.io.PrintWriter;
-import org.junit.Test; 
+
+import org.junit.Test;
 import org.junit.Before; 
 import org.junit.After;
 import static org.junit.Assert.*;
@@ -51,7 +51,7 @@ public class DictionaryTrieTest {
 		// search the scramble
 		int rows = 4;
 		int cols = 4;
-		String scrambleStr = "fxie amlo ewbx astu";
+		String scrambleStr = "gaum oaeu dnbr siaf";
 		String[] inter = scrambleStr.split(" ");
 		char[][] scramble = new char[rows][cols];
 		for (int i = 0; i < rows; i++) {
@@ -59,9 +59,21 @@ public class DictionaryTrieTest {
 		}
 
 		List<String> scrambleWords = testClass.searchWordsInScramble(scramble, rows, cols);
+		Set<String> set = new HashSet<>(scrambleWords);
 
-		for (String str : scrambleWords)
-			System.out.println(str);
+		scrambleWords = new ArrayList<>(set);
+
+		Collections.sort(scrambleWords, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+//				return o2.length() - o1.length();
+				return o1.compareTo(o2);
+			}
+		});
+
+		for (String word : scrambleWords) {
+			System.out.println(word);
+		}
 
 		System.out.println("TOTAL words " + scrambleWords.size());
 
