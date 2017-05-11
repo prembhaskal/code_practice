ca84a0fbd1bffef7bcfda17ef1002527d5bc4d21
bhaskal <premkumar.bhaskal@nokia.com>
2017-05-11 18:16:29 +0530

    tried a websocket example from java mag nov-2015

---
+++ code_pratice_ws/javaee/websocket_test/src/main/java/com/prem/ClockServer.java
@@ -0,0 +1,56 @@
+package com.prem;
+
+import javax.websocket.*;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@ServerEndpoint("/clock")
+public class ClockServer {
+
+	Thread updateThread;
+	boolean running = false;
+
+	@OnOpen
+	public void startClock(Session session) {
+		final Session mySession = session;
+		this.running = true;
+		final SimpleDateFormat sdf = new SimpleDateFormat("h:mm:ss a");
+		this.updateThread = new Thread() {
+			public void run() {
+				while (running) {
+					String dateString = sdf.format(new Date());
+					try {
+						mySession.getBasicRemote().sendText(dateString);
+						sleep(1000);
+					} catch (IOException | InterruptedException ie) {
+						running = false;
+					}
+				}
+			}
+		};
+		this.updateThread.start();
+	}
+
+	@OnMessage
+	public String handleMessage(String incomingMessage) {
+		if ("stop".equals(incomingMessage)) {
+			this.stopClock();
+			return "clock stopped";
+		} else {
+			return "unknown message: " + incomingMessage;
+		}
+	}
+
+	@OnError
+	public void clockError(Throwable t) {
+		this.stopClock();
+	}
+
+	@OnClose
+	public void stopClock() {
+		this.running = false;
+		this.updateThread = null;
+	}
+}
