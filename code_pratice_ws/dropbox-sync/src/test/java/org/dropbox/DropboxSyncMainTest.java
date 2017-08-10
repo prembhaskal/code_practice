3d8a0f20a4f763c8c289760cd4dab9d23cda38de
prem <pbhaskal@gmail.com>
2017-08-10 18:36:43 +0530

    creating a zip for production having lib, scripts and conf directory.

--- code_pratice_ws/dropbox-sync/src/test/java/org/dropbox/DropboxSyncMainTest.java
+++ code_pratice_ws/dropbox-sync/src/test/java/org/dropbox/DropboxSyncMainTest.java
@@ -6,10 +6,9 @@ public class DropboxSyncMainTest {
 
 	@Test
 	public void testSync() throws Exception {
-		System.setProperty("https.proxyHost", "10.144.8.20");
+		System.setProperty("https.proxyHost", "87.254.212.120");
 		System.setProperty("https.proxyPort", "8080");
 //		curl https://api.dropbox.com/1/account/info -H "Authorization:Bearer 1N5AsbT_-N4AAAAAAAACvEtZ8KXWOupMVAGbI6tK27-rcyjSfrMJkmCw3z8kOjPN"
-//		new DropboxSyncMain().setUpAndSync("1N5AsbT_-N4AAAAAAAACuC7yby6f16y3K5PrmSlHYGCzy-N4LaH6lr8LfeaejF1p");
-		new DropboxSyncMain().setUpAndSync("1N5AsbT_-N4AAAAAAAACvEtZ8KXWOupMVAGbI6tK27-rcyjSfrMJkmCw3z8kOjPN");
+		new DropboxSyncMain().setUpAndSync("1N5AsbT_-N4AAAAAAAACuC7yby6f16y3K5PrmSlHYGCzy-N4LaH6lr8LfeaejF1p");
 	}
 }
