c282e9c18dc0096ba4acb038e1fa239054be737b
prembhaskal <pbhaskal@gmail.com>
2022-11-04 20:09:16 +0530

    Java question after long time

---
+++ code_pratice_ws/pratice_code/src/leetcode/misc/P345ReverseVowels.java
@@ -0,0 +1,50 @@
+package leetcode.misc;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class P345ReverseVowels {
+    public static void main(String[] args) {
+        P345ReverseVowels sol = new P345ReverseVowels();
+        String input = "leetcode";
+        String out = sol.reverseVowels(input);
+        System.out.printf("input: %s, output: %s\n", input, out);
+    }
+    public String reverseVowels(String s) {
+
+        char[] vowels = new char[]{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        Map<Character, Integer> vmap = new HashMap<>();
+        for (char ch : vowels) {
+            vmap.put(ch, 1);
+        }
+
+        char[] chars = s.toCharArray();
+
+        int sptr = 0;
+        int eptr = chars.length - 1;
+
+        for (; sptr < chars.length && eptr >= 0 && sptr < eptr; ) {
+            char left = chars[sptr];
+            if (!vmap.containsKey(left)) {
+                sptr++;
+                continue;
+            }
+
+            char right = chars[eptr];
+            if (!vmap.containsKey(right)) {
+                eptr--;
+                continue;
+            }
+
+            // exchange
+            chars[sptr] = right;
+            chars[eptr] = left;
+            sptr++;
+            eptr--;
+        }
+
+        return new String(chars);
+    }
+
+
+}
