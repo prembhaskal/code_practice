68c227851fe14351c53bba42ca0a3beeb9cb4469
prembhaskal <pbhaskal@gmail.com>
2023-05-19 19:15:35 +0530

    game from HF

--- code_pratice_ws/pratice_code/src/main/java/basics/ScratchPad.java
+++ code_pratice_ws/pratice_code/src/main/java/basics/ScratchPad.java
@@ -13,3 +13,44 @@ public class ScratchPad {
         System.out.printf("word's list length is %d \n", wordlist.length);
     }
 }
+
+class SinkStartUpGame {
+    private int[] locs;
+
+    public SinkStartUpGame(int[] locs) {
+        this.locs = locs;
+    }
+
+    public String checkGuess(int guess) {
+        for (int i = 0; i < locs.length; i++) {
+            if (locs[i] == guess) {
+                return "success";
+            }
+        }
+        return "failed";
+    }
+
+    public boolean isDone() {
+        return false;
+    }
+
+}
+
+class SinkStartUpGameTest {
+    public static void main(String[] args) {
+        var locations = new int[]{2, 4, 5};
+        var game = new SinkStartUpGame(locations);
+
+        var res = game.checkGuess(2);
+        if (res.equals("success")) {
+            System.out.printf("working\n");
+        } else {
+            System.out.printf("not working\n");
+            fail();
+        }
+    }
+
+    static void fail() {
+        throw new RuntimeException("test failed");
+    }
+}
