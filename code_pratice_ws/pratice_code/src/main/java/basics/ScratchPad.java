d3ce32ce3fc849c99fca7066da4cedc82e83ff35
prembhaskal <pbhaskal@gmail.com>
2023-05-19 19:30:27 +0530

    added more features

--- code_pratice_ws/pratice_code/src/main/java/basics/ScratchPad.java
+++ code_pratice_ws/pratice_code/src/main/java/basics/ScratchPad.java
@@ -1,6 +1,8 @@
 package basics;
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
 public class ScratchPad {
 
@@ -16,14 +18,35 @@ public class ScratchPad {
 
 class SinkStartUpGame {
     private int[] locs;
+    private int guesses;
+    private int rem;
+
+    private boolean isDone;
 
     public SinkStartUpGame(int[] locs) {
         this.locs = locs;
+        this.rem = locs.length;
     }
 
     public String checkGuess(int guess) {
+        if (isDone) {
+            System.out.println("game already over");
+            return "success";
+        }
+        guesses++;
+
+        if (guess < 0) {
+            System.err.printf("invalid guess: %d\n", guess);
+            return "failure";
+        }
+
         for (int i = 0; i < locs.length; i++) {
             if (locs[i] == guess) {
+                locs[i] = -1;
+                rem--;
+                if (rem == 0) {
+                    isDone = true;
+                }
                 return "success";
             }
         }
@@ -31,23 +54,51 @@ class SinkStartUpGame {
     }
 
     public boolean isDone() {
-        return false;
+        return isDone;
+    }
+
+    public void printSummary() {
+        if (isDone) {
+            System.out.printf("game over, total guess: %d\n", guesses);
+        } else {
+            System.out.printf("game not over, total guess: %d\n", guesses);
+        }
     }
 
 }
 
 class SinkStartUpGameTest {
     public static void main(String[] args) {
-        var locations = new int[]{2, 4, 5};
+        int startloc =  new Random().nextInt(0, 4);
+        var locations = new int[3];
+        for (int i = 0; i < 3; i++) {
+            locations[i] = startloc;
+            startloc++;
+        }
+
         var game = new SinkStartUpGame(locations);
 
-        var res = game.checkGuess(2);
-        if (res.equals("success")) {
-            System.out.printf("working\n");
-        } else {
-            System.out.printf("not working\n");
-            fail();
+        while (!game.isDone()) {
+            int userinput = readUserInput();
+            var res = game.checkGuess(userinput);
+            System.out.printf("guess: %d, result: %s\n", userinput, res);
         }
+
+        game.printSummary();
+
+//        var res = game.checkGuess(2);
+//        if (res.equals("success")) {
+//            System.out.printf("working\n");
+//        } else {
+//            System.out.printf("not working\n");
+//            fail();
+//        }
+    }
+
+    static int readUserInput() {
+        System.out.printf("enter a number: ");
+        Scanner scan = new Scanner(System.in);
+        return scan.nextInt();
     }
 
     static void fail() {
