de6e90d077550102a33e3322d5226f707f70510c
prembhaskal <pbhaskal@gmail.com>
2023-07-10 22:59:24 +0530

    thread practice

--- code_pratice_ws/practice_code/src/main/java/basics/ScratchPad.java
+++ code_pratice_ws/practice_code/src/main/java/basics/ScratchPad.java
@@ -1,29 +1,45 @@
 package basics;
 
-import java.util.*;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.stream.IntStream;
 
 public class ScratchPad {
 
-    public static void main(String[] s) {
-        System.out.printf("hello world\n");
-
-        List<Integer> list = new ArrayList<>();
-        list.add(1);
-        list.add(3);
-        list.add(6);
-        list.add(-1);
-        list.add(2);
-
-        Collections.sort(list);
-
-        printf("sorted list: %s\n", list);
-
-        Collections.sort(list, (o1, o2) -> o2 - o1);
-
-        printf("reverse sorted list: %s\n", list);
+    public static void main(String[] args) {
+        try {
+            runMain(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public static void printf(String msg, Object ...s) {
+    public static void runMain(String[] args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(1);
+        Callable<Integer> task = new Callable<Integer>() {
+
+            @Override
+            public Integer call() {
+                printf("printing from thread ... \n");
+                return 1;
+            }
+        };
+
+        Future<?> future = executorService.submit(task);
+        executorService.shutdown();
+
+        var ans = future.get();
+
+//        Thread.currentThread().sleep(200);
+
+        printf("printing from main ... \n");
+        printf("result from thread: %d\n", ans);
+
+    }
+
+    public static void printf(String msg, Object... s) {
         System.out.printf(msg, s);
     }
 }
