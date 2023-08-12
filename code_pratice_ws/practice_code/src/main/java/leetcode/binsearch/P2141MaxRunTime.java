7723a96f4dfadc7d6644300074b54f351714095c
prembhaskal <pbhaskal@gmail.com>
2023-08-12 20:55:38 +0530

    saving changes

--- code_pratice_ws/practice_code/src/main/java/leetcode/binsearch/P2141MaxRunTime.java
+++ code_pratice_ws/practice_code/src/main/java/leetcode/binsearch/P2141MaxRunTime.java
@@ -1,39 +1,39 @@
 package leetcode.binsearch;
 
 public class P2141MaxRunTime {
-        public long maxRunTime(int n, int[] batteries) {
-            long totalsum = 0;
-            for (int bat : batteries) {
-                totalsum = totalsum + bat;
-            }
-            long low = 1;
-            long high = totalsum / n;
+    public long maxRunTime(int n, int[] batteries) {
+        long totalsum = 0;
+        for (int bat : batteries) {
+            totalsum = totalsum + bat;
+        }
+        long low = 1;
+        long high = totalsum / n;
 
-            long ans = -1;
-            while(low <= high) {
-                long mid = low + (high - low) / 2;
-                if (canReachTarget(n, batteries, mid)) {
-                    ans = mid;
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
-                }
+        long ans = -1;
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            if (canReachTarget(n, batteries, mid)) {
+                ans = mid;
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
-
-            return ans;
         }
 
-        boolean canReachTarget(int n, int[] batt, long target) {
-            long totalavail = 0;
-            for (int bat : batt) {
-                totalavail = totalavail + min(bat, target);
-            }
+        return ans;
+    }
 
-            long need = (long)n * target;
-            return need <= totalavail;
+    boolean canReachTarget(int n, int[] batt, long target) {
+        long totalavail = 0;
+        for (int bat : batt) {
+            totalavail = totalavail + min(bat, target);
         }
 
-        long min(long a, long b ) {
-            return a < b ? a : b;
-        }
+        long need = (long) n * target;
+        return need <= totalavail;
+    }
+
+    long min(long a, long b) {
+        return a < b ? a : b;
+    }
 }
