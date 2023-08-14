6781cdf5c80b7e05845757dd32b201bb83ceb851
prembhaskal <pbhaskal@gmail.com>
2023-08-14 13:21:18 +0530

    heap based

--- code_pratice_ws/practice_code/src/main/java/leetcode/array/P215KthLargest.java
+++ code_pratice_ws/practice_code/src/main/java/leetcode/array/P215KthLargest.java
@@ -1,13 +1,29 @@
 package leetcode.array;
 
 import java.util.Random;
+import java.util.PriorityQueue;
 
 public class P215KthLargest {
     public int findKthLargest(int[] nums, int k) {
+//        return heapbased(nums, k);
         return quickselect(nums, 0, nums.length - 1, k - 1);
     }
+    
+    // slower O(klogk)
+    int heapbased(int[] nums, int k) {
+        var heap = new PriorityQueue<Integer>();
+        for (int num : nums) {
+            heap.add(num);
 
-    // quick select in descending order,
+            if (heap.size() > k) {
+                heap.remove();
+            }
+        }
+        return heap.peek();
+    }
+
+    // quick select in descending order.
+    // O(n)
     int quickselect(int[] nums, int start, int end, int k) {
         int pivot = new Random().nextInt(end - start + 1) + start;
         swap(nums, start, pivot);
