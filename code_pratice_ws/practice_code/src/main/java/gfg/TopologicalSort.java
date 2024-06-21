a68a2ddfe43bc7e8ee840421945057dd7fefb5fd
premkumar bhaskal <pbhaskal@gmail.com>
2024-06-21 20:28:11 +0530

    topo sort in java

--- code_pratice_ws/practice_code/src/main/java/gfg/TopologicalSort.java
+++ code_pratice_ws/practice_code/src/main/java/gfg/TopologicalSort.java
@@ -52,4 +52,48 @@ public class TopologicalSort {
         return topolist;
     }
 
+    static int[] topolist1;
+    static int index = 0;
+
+    static int[] topologicalDFS(int V, ArrayList<ArrayList<Integer>> adj) {
+        topolist1 = new int[V];
+        index = 0;
+        var visited = new int[V];
+        for (int i = 0; i < V; i++) {
+            dfsColor(i, visited, adj);
+        }
+        // reverse list
+        reverse(topolist1);
+        return topolist1;
+    }
+
+    static void reverse(int[] ar) {
+        int i = 0;
+        int j = ar.length - 1;
+        while (i < j) {
+            int temp = ar[i];
+            ar[i] = ar[j];
+            ar[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+
+    static void dfsColor(int curr, int[] visited, ArrayList<ArrayList<Integer>> adjList) {
+        if (visited[curr] == 2) {
+            // already visited previously
+            return;
+        }
+
+        visited[curr] = 1; // mark grey , visiting
+        for (int next : adjList.get(curr)) {
+            dfsColor(next, visited, adjList);
+        }
+        visited[curr] = 2; // mark black
+        topolist1[index] = curr;
+        index++;
+    }
+
+
 }
